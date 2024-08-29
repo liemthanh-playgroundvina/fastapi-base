f6dd65bf4c0e9f55db31929c3b859fde8312d69b
liemthanhplayground <liemthanh@playgroundvina.com>
2024-08-29 08:08:59 +0700

    add chatbot

--- scripts/app/entrypoint.sh
+++ scripts/app/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+python scripts/add_llm.py
+
 uvicorn app.main:app --host "${BASE_HOST}" --port "${BASE_PORT}" &
 
 python scripts/create_super_user.py
-
-python scripts/add_llm.py
 
 wait
