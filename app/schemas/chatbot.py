749903ebb8b52623d907de8ae1f85310ebd5ba77
liemthanhplayground <liemthanh@playgroundvina.com>
2024-08-29 13:46:56 +0700

    add chatbot

--- app/schemas/chatbot.py
+++ app/schemas/chatbot.py
@@ -10,9 +10,29 @@ from app.helpers.llm.preprompts.store import STORES
 with open(os.path.join(settings.STATIC_URL, "files/app", "chatbot.json"), 'r') as f:
     model_data = json.load(f)
 
+class ChatModel(BaseModel):
+    platform: str
+    model_name: str
+    temperature: float
+    max_tokens: int
+
+    def validate(self) -> None:
+        if self.platform not in model_data:
+            raise CustomException(http_code=400, code='400', message=f"Unsupported platform '{self.platform}'.")
+
+        if self.model_name not in model_data[self.platform]:
+            raise CustomException(http_code=400, code='400', message=f"Unsupported model '{self.model_name}' for platform '{self.platform}'.")
+
+        if not (0 <= self.temperature <= 1.0):
+            raise CustomException(http_code=400, code='400', message=f"Temperature must be between 0.0 and 1.0.")
+
+        max_token_limit = model_data[self.platform][self.model_name]
+        if not (256 <= self.max_tokens <= max_token_limit):
+            raise CustomException(http_code=400, code='400', message=f"max_tokens must be between 256 and {max_token_limit}.")
+
 class BaseChatRequest(BaseModel):
     messages: List[Dict[str, Union[str, List[Dict[str, Union[str, Dict[str, str]]]]]]]
-    chat_model: Dict[str, any]
+    chat_model: ChatModel
 
     @classmethod
     def validate_messages(cls, messages: List[Dict[str, Union[str, List[Dict[str, Union[str, Dict[str, str]]]]]]]) -> None:
@@ -21,31 +41,12 @@ class BaseChatRequest(BaseModel):
             if msg['role'] not in valid_roles:
                 raise CustomException(http_code=400, code='400', message=f"Invalid role '{msg['role']}' in messages.")
 
-    @classmethod
-    def validate_chat_model(cls, chat_model: Dict[str, any]) -> None:
-        required_fields = ["platform", "model_name", "temperature", "max_tokens"]
-        for field in required_fields:
-            if field not in chat_model:
-                raise CustomException(http_code=400, code='400', message=f"Missing field '{field}' in chat_model.")
-
-        platform = chat_model['platform']
-        model_name = chat_model['model_name']
-        temperature = chat_model['temperature']
-        max_tokens = chat_model['max_tokens']
-
-        if platform not in model_data:
-            raise CustomException(http_code=400, code='400', message=f"Unsupported platform '{platform}'.")
-
-        if model_name not in model_data[platform]:
-            raise CustomException(http_code=400, code='400', message=f"Unsupported model '{model_name}' for platform '{platform}'.")
-
-        if not (0 <= temperature <= 1.0):
-            raise CustomException(http_code=400, code='400', message=f"Temperature must be between 0.0 and 1.0.")
-
-        max_token_limit = model_data[platform][model_name]
-        if not (256 <= max_tokens <= max_token_limit):
-            raise CustomException(http_code=400, code='400', message=f"max_tokens must be between 256 and {max_token_limit}.")
-
+    @root_validator(pre=True)
+    def validate(cls, values):
+        cls.validate_messages(values['messages'])
+        chat_model = ChatModel(**values.get('chat_model', {}))
+        chat_model.validate()
+        return values
 
 class ChatRequest(BaseChatRequest):
     store_name: Optional[str] = ""
@@ -69,22 +70,43 @@ class ChatRequest(BaseChatRequest):
             }
         }
 
-    @root_validator()
+    @root_validator(pre=True)
     def validate(cls, values):
-        # Message
-        cls.validate_messages(values['messages'])
-        # Store
+        super().validate(values)
         store_name = values.get('store_name', "")
         if store_name.strip() and store_name not in STORES:
             raise CustomException(http_code=400, code='400', message=f"Invalid store name '{store_name}'.")
-        # Model
-        cls.validate_chat_model(values.get('chat_model', {}))
         return values
 
-
 class ChatVisionRequest(BaseChatRequest):
-    @root_validator()
+    class Config:
+        schema_extra = {
+            "example": {
+                "messages": [
+                    {"role": "system", "content": "You are an assistant."},
+                    {"role": "user", "content": "Xin chào"},
+                    {"role": "assistant", "content": "Chào bạn. Tôi có thể giúp gì cho bạn?"},
+                    {"role": "user", "content": [
+                        {
+                            "type": "text",
+                            "text": "Bạn có thể mô tả hình ảnh này?",
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {"url": "https://images.unsplash.com/photo-1582538885592-e70a5d7ab3d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80"},
+                        }
+                    ]}
+                ],
+                "chat_model": {
+                    "platform": "OpenAI",
+                    "model_name": "gpt-4o",
+                    "temperature": 0.7,
+                    "max_tokens": 2048,
+                },
+            }
+        }
+
+    @root_validator(pre=True)
     def validate(cls, values):
-        cls.validate_messages(values['messages'])
-        cls.validate_chat_model(values.get('chat_model', {}))
+        super().validate(values)
         return values
